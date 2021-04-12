8125a67dc5c444a5fb7dd20e0842799c2d8f97e4
Artem <artem.pif@mail.ru>
2021-04-12 17:39:11 +0300

    fix

--- renderers.py
+++ renderers.py
@@ -2,14 +2,14 @@ from rest_framework.renderers import BrowsableAPIRenderer
 
 
 class GETFormBrowsableAPIRenderer(BrowsableAPIRenderer):
-	template = 'browesable-api-template.html'
+    """ Кастомный рендерер, добавляющий в BrowsableAPIRenderer поле для ввода GET параметров """
+    template = 'browsable-api-template.html'
 
-	def get_context(self, data, accepted_media_type, renderer_context):
-		context = super().get_context(data, accepted_media_type, renderer_context)
-		view = renderer_context['view']
-		if 'GET' in view.allowed_methods:
-			sz_class = view.get_serializer_class()
-			sz = sz_class(data=view.request.query_params)
-			sz.is_valid(raise_exception=True)
-			context['get_form'] = self.render_form_for_serializer(sz)
-		return context
+    def get_context(self, data, accepted_media_type, renderer_context):
+        context = super().get_context(data, accepted_media_type, renderer_context)
+        view = renderer_context['view']
+        if 'GET' in view.allowed_methods:
+            sz_class = view.get_serializer_class() if hasattr(view, 'get_serializer_class') else view.serializer_class
+            sz = sz_class(data=view.request.query_params)
+            context['get_form'] = self.render_form_for_serializer(sz)
+        return context
